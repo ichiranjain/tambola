c22d0d08974eba6b508e181e180349ffc91ab172
Vedant Raiththa <vedantraiththa@gmail.com>
2020-04-26 00:59:37 -0700

    Refactor

--- script.js
+++ script.js
@@ -1,96 +1,101 @@
-$(document).ready(function () {
-	let para = document.getElementById("number_para");
-	let btnNextNumber = document.getElementById("random");
-	let table = document.getElementById("table");
-	let game = document.getElementById("end");
-	let btnNewGame = document.getElementById("btn_new-game");
-	let $cache = $("#cache");
+/**
+ * Pop a random element from the array.
+ * Note: This changes the state of the given array.
+ */
+Array.prototype.popRandom = function () {
+	const selectedIndex = Math.floor(Math.random() * this.length);
+	const [selectedItem] = this.splice(selectedIndex, 1);
+	return selectedItem;
+};
 
-	let tokens = new Array();
-	let store_num = new Array();
-	let len_store = 0;
-	let len = 90;
+$(document).ready(function () {
+	const $para = $("#current-number");
+	const $btnNextNumber = $("#btn-next-number");
+	const table = document.getElementById("table");
+	const $gameOver = $("#game-over");
+	const $btnNewGame = $("#btn-new-game");
+	const $cache = $("#cache");
+
+	let availableTokens, foundTokens;
+
+	let row_prev, column_prev, row, column;
 
 	function init() {
-		$cache.empty();
+		row_prev = -1;
+		column_prev = -1;
 
-		for (let i = 1; i <= 90; ++i) {
-			tokens.push(i);
+		availableTokens = new Array();
+		foundTokens = new Array();
+
+		const START_RANGE = 1;
+		const END_RANGE = 90;
+
+		// set available tokens
+		for (let i = START_RANGE; i <= END_RANGE; i++) {
+			availableTokens.push(i);
 		}
+
+		// setup UI
+		$cache.empty();
+		$para.text("-");
+
+		// set board
+		for (var i = 0; i < 9; ++i) {
+			for (var j = 0; j < 10; j++) {
+				table.rows[i].cells[j].innerText = "";
+				table.rows[i].cells[j].style.background = null;
+			}
+		}
+
+		$btnNextNumber.show();
+		$gameOver.hide();
 	}
 
 	//var w = window.speechSynthesis;
 
-	for (var i = 0; i < 9; ++i) {
-		for (var j = 0; j < 10; j++) {
-			table.rows[i].cells[j].innerText = "";
-		}
-	}
-
 	init();
 
-	var row_prev = -1;
-	var column_prev = -1;
-	var row, column;
+	$btnNextNumber.click(function () {
+		// get random number from available tokens
+		const currentToken = availableTokens.popRandom();
 
-	btnNextNumber.addEventListener("click", function () {
-		var ran_value = Math.floor(Math.random() * len);
-		len -= 1;
-		var n = tokens[ran_value].toString();
-		store_num.push(tokens[ran_value]);
-		len_store += 1;
-		var msg = new SpeechSynthesisUtterance(n);
+		foundTokens.push(currentToken);
+
+		// Show the selected token in previous number
+		$cache.append(`<li class="list-inline-item">${currentToken}</li>`);
+
+		// announce the selected token
+		var msg = new SpeechSynthesisUtterance(currentToken);
 		msg.pitch = 2;
 		//w.speak(msg);
-		row = parseInt(tokens[ran_value] / 10);
-		column = parseInt(tokens[ran_value] % 10);
+
+		// place the selected token
+		row = parseInt(currentToken / 10);
+		column = parseInt(currentToken % 10);
 		if (column == 0) {
 			column = 10;
 			row -= 1;
 		}
 
-		$cache.append(
-			`<li class="list-inline-item">${store_num[store_num.length - 1]}</li>`
-		);
-
-		table.rows[row].cells[column - 1].innerText = n;
+		table.rows[row].cells[column - 1].innerText = currentToken;
 		table.rows[row].cells[column - 1].style.backgroundColor = "#d4c893";
 		table.rows[row].cells[column - 1].style.backgroundImage =
 			"linear-gradient(45deg, #FFFF00 0%, #d4c893 100%)";
 		if (row_prev !== -1 && column_prev !== -1) {
 			table.rows[row_prev].cells[column_prev - 1].style.background = null;
 		}
-		para.innerText = n;
+		$para.text(currentToken);
 		row_prev = row;
 		column_prev = column;
-		tokens.splice(ran_value, 1);
-		if (len == 0) {
-			btnNextNumber.style.display = "none";
-			game.style.display = "block";
+		if (availableTokens.length == 0) {
+			$btnNextNumber.hide();
+			$gameOver.show();
 		}
 	});
 
-	btnNewGame.addEventListener("click", function () {
+	$btnNewGame.click(function () {
 		if (confirm("Are you sure you want to reset the game?")) {
-			btnNextNumber.style.display = "block";
-			game.style.display = "none";
-			para.innerText = "0";
-			tokens = new Array();
-			for (var i = 1; i <= 90; ++i) {
-				tokens.push(i);
-			}
-			store_num = new Array();
-			len_store = 0;
-
-			for (var i = 0; i < 9; ++i) {
-				for (var j = 0; j < 10; j++) {
-					table.rows[i].cells[j].innerText = "";
-				}
-			}
 			init();
-			table.rows[row].cells[column - 1].style.background = null;
-			var row_prev = -1;
-			var column_prev = -1;
 		}
 	});
 });
